926b1e45a1424c99ef9797e8f26eae315860733c
Michael Leveille <leveille1313@gmail.com>
2021-02-20 20:08:45 -0500

    Removed some code

--- my_test.py
+++ my_test.py
@@ -1,17 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-
-print(sys.version)
-print(sys.executable)
-
-def greet(who):
-    greeting = "Hello {}".format(who)
-    return greeting
-
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
